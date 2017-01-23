cff4c18d7bc9a9f56964aecde0b8149f82c00726
Maggie Harrington and Patrick McGreevy <patrick@>
2017-01-23 10:34:56 -0800

    Create Place constructor

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// Business logic
+function Place(name, location, landmarks, time, notes) {
+  this.name = name;
+  this.location = location;
+  this.landmarks = landmarks;
+  this.time = time;
+  this.notes = notes;
+}
+
+// Interface logic
